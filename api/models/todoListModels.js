726d182d61181b9761f1abd0a61bc6fc8049963e
root <nassimaskri@gmail.com>
2017-10-06 00:38:34 +0100

    setController

---
+++ api/models/todoListModels.js
@@ -0,0 +1,22 @@
+let mongoose = require('mongoose')
+let schema = mongoose.Schema
+
+let taskSchema = new Schema({
+    name : {
+      type :String,
+      required: 'enter the name of the task'
+    },
+    Created_day : {
+        type : Date,
+        default : Date.now
+    },
+    status : {
+        type : [{
+            type : String,
+            enum : ['pending','Ongoing','completed']
+        }],
+        default : 'pending'
+    }
+})
+
+module.exports = mongoose.model('Tasks',taskSchema)
