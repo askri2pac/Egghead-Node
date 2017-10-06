126eb866bfc4edec97d95a3c2d9d9f84612eba3d
xtend <askri@xtendplex.com>
2017-10-06 11:42:15 +0100

    merge server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let express = require('express'),
 app = express(),
 port = process.env.PORT || 3000
@@ -6,12 +6,3 @@ port = process.env.PORT || 3000
 app.listen(port,()=>{
     console.log("on est sur le port 3000")
 })
-=======
-var express = require("express"),
-app = express(),
-port = process.env.PORT || 3000
-
-app.listen(port)
-
-console.log("listening on port" + port)
->>>>>>> origin/master
